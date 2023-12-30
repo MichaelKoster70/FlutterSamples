9fea44029a2102592ccc70e9e30c0912da1c9e42
Michael Koster <MichaelKoster70@users.noreply.github.com>
2023-12-30 13:47:52 +0100

    added scripts

---
+++ eng/Create-DevCert.ps1
@@ -0,0 +1,15 @@
+﻿# ----------------------------------------------------------------------------
+# <copyright company="Michael Koster">
+#   Copyright (c) Michael Koster. All rights reserved.
+#   Licensed under the MIT License.
+# </copyright>
+# ----------------------------------------------------------------------------
+# Create Self Signed x.509 Cert for APPX Development
+# ----------------------------------------------------------------------------
+
+$cert = New-SelfSignedCertificate -Type Custom -Subject "CN=Michael Koster" -KeyUsage DigitalSignature -FriendlyName "APPX Dev Cert Michael Koster" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+$credentials = Get-Credential -User "PFX File" -Message "Enter password for PFX file"
+$thumbPrint = $cert.Thumbprint
+Export-PfxCertificate -cert "Cert:\CurrentUser\My\$thumbPrint" -FilePath DevCert.pfx -Password $credentials.Password
+Export-Certificate -cert "Cert:\CurrentUser\My\$thumbPrint" -FilePath DevCert.cer
