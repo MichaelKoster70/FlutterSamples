e3893473a83261c01cdbc6fd513d63fafada5a6d
Michael Koster <MichaelKoster70@users.noreply.github.com>
2024-02-05 20:53:08 +0100

    Added a simple example

--- windows_event_log/windows_event_log/example/lib/main.dart
+++ windows_event_log/windows_event_log/example/lib/main.dart
@@ -10,28 +10,28 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 import 'package:windows_event_log/event_log_consumer.dart';
 import 'package:windows_event_log/event_log_provider.dart';
 import 'package:windows_event_log_example/events.dart';
 
 void main() {
-  final app = WindowsEventLog.application;
+  final provider =
+      WindowsEventLogProviderFactory.create(Events.sampleProviderId);
 
-  final provider = WindowsEventLogProviderFactory.create(sampleProviderId);
-
-  provider.writeEventWithString(logApplicatonInfo, "Sample Info Event");
+  provider.writeEventWithString(Events.logApplicatonInfo, "Sample Info Event");
 
   // Just for debugging purposes, print all events
-  for (final event in app.allEvents) {
-    print(event.message);
-    print('------------');
-    print(event.xml.toString());
-    print('============');
-  }
+  //
+  // final app = WindowsEventLog.application;
+  // for (final event in app.allEvents) {
+  //   print(event.message);
+  //   print('------------');
+  //   print(event.xml.toString());
+  //   print('============');
+  // }
 
-  //runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -42,56 +42,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> sumAsyncResult;
-
+  final app = WindowsEventLog.application;
+  final allEvents = List<EventRecord>.empty(growable: true);
   @override
   void initState() {
     super.initState();
+
+    allEvents.addAll(app.allEvents);
   }
 
   @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
+    const textStyle = TextStyle(fontSize: 10);
+    const spacerSmall = SizedBox(height: 2);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Native Packages'),
+          title: const Text('Windows Event Viewer'),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
+        body: ListView.builder(
+          itemCount: allEvents.length,
+          itemBuilder: (context, index) {
+            final event = allEvents[index];
+            return Column(
+              children: <Widget>[
+                Text(
+                  event.message,
                   style: textStyle,
-                  textAlign: TextAlign.center,
+                  textAlign: TextAlign.left,
                 ),
                 spacerSmall,
                 Text(
-                  'sum(1, 2) = $sumResult',
+                  event.xml.toString(),
                   style: textStyle,
-                  textAlign: TextAlign.center,
+                  textAlign: TextAlign.left,
                 ),
                 spacerSmall,
-                FutureBuilder<int>(
-                  future: sumAsyncResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'await sumAsync(3, 4) = $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
+                const Divider(
+                  color: Colors.black,
+                  height: 5,
+                  thickness: 5,
+                  indent: 20,
+                  endIndent: 0,
                 ),
               ],
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
--- windows_event_log/windows_event_log_generator/README.md
+++ windows_event_log/windows_event_log_generator/README.md
@@ -1,39 +1,38 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+Implements the matchung code generator for the windows_event_log package to automate the generation of event descriptor.
+This enables you to automatically keep the manifest file in sync with the DART code.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+* Generates event descriptor entries read from a manifest based Windows ETW provider.
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
+Add the following dev dependency to your pubspec.yaml file
 
+```yaml
+dev_dependencies:
+  build_runner: ^2.4.0
+  windows_event_log_generator:
+    path: ../../windows_event_log_generator
+```    
+
+Add the @ManifestEvents annotation to a mantching class
 ```dart
-const like = 'sample';
+@ManifestEvents(manifestPath: 'windows\\data\\sample.man')
+final class Events {
+  const Events._();
+}
 ```
 
-## Additional information
+Run the generator using build_runner
+```
+dart run build_runner build
+```
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+
+
+
+
+
